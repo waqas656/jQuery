a11d92d969e622da8068d9cda77a2e5c37128f2f
Waqas Ahmed <waqasmlik565@gmail.com>
2024-03-04 01:45:01 +0500

    jquery

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
+$("h1")
+.css("color", "green")
+.css("font-size", "10rem")
+.css("text-align", "center")
+.css("margin-top", "5px");
 
-$("h1").css("color", "red");
+$(document).keydown(function(event) {
+    $("h1").text(event.key.toUpperCase());
+});
+
+$("h1").on("mouseover", function() {
+    $("h1").css("color", "red");
+})
+
+$("h1").on("mouseout", function() {
+    $("h1").css("color", "green");
+})
 
 
